0dc722a409433efbe65b92486853ecaabb2827d5
Yeondi <atlantis2568@gmail.com>
2020-06-28 15:59:28 +0900

    ammo
    ammo 중간 업로드

---
+++ Assets/Scripts/MonoBehaviour/Ammo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    public int damageInflicted;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision is BoxCollider2D)
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+
+            StartCoroutine(player.DamageCharacter(damageInflicted, 0.0f));
+
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/MonoBehaviour/Weapon.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject ammoPrefab;
+    static List<GameObject> ammoPool;
+    public int poolSize;
+
+    private void Awake()
+    {
+        if(ammoPool == null)
+        {
+            ammoPool = new List<GameObject>();
+        }
+
+        for(int i=0;i<poolSize;i++)
+        {
+            GameObject ammoObject = Instantiate(ammoPrefab);
+            ammoObject.SetActive(false);
+            ammoPool.Add(ammoObject);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            FireAmmo();
+        }
+    }
+
+    GameObject SpawnAmmo(Vector3 location)
+    {
+        foreach(GameObject ammo in ammoPool)
+        {
+            if(ammo.activeSelf == false)
+            {
+                ammo.SetActive(true);
+                ammo.transform.position = location;
+                return ammo;
+            }
+        }
+        return null;
+    }
+
+    private void FireAmmo()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        ammoPool = null;
+    }
+}
